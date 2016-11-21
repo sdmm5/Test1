a0004671d297084ed421b4ed62091c079c6881f5
sdmm5 <sdmm5at@gmail.com>
2016-11-21 20:25:28 +0800

    Initial Commit

---
+++ Test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test1
+//
+//  Created by lip on 16/11/21.
+//  Copyright © 2016年 com.sdmm5at. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test1
+//
+//  Created by lip on 16/11/21.
+//  Copyright © 2016年 com.sdmm5at. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
